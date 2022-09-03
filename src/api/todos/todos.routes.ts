efccf608a2d8bd72b119adad495387c23a76026d
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-09-03 13:36:51 -0300

    Init routes and todos model

--- src/api/todos/todos.routes.ts
+++
@@ -1,7 +0,0 @@
-import {Router} from "express"
-import * as TodoHandler from "./todos.handler"
-
-const router =  Router()
-router.get("/", TodoHandler.findAll)
-
-export default router
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,34 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet'
+import morgan from 'morgan'
 
-import api from './api';
+
 import MessageResponse from './interfaces/MessageResponse';
+import api from './routes';
+import * as middlewares from './middlewares';
 
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const {PORT = 5000} = process.env;
 
+app.use(morgan('dev'));
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/v1', api);
+
 app.get<{}, MessageResponse>('/', (req, res) => {
-  res.json({
-    message: "It's working just Fine!"
-  });
+  res.json({message: "It's working just Fine!"});
 });
 
-app.use('/api', api);
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
-app.listen(port, () => {
-  console.log(`Server Listening: http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server Listening: http://localhost:${PORT}`);
 });
+
+export default app;
